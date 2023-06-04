1b903fd18767eab7167923ff3afc883dc3915e62
Anup Kumar M <anupkumarmofficial@gmail.com>
2023-06-04 17:10:32 +0530

    The order can have multiple products added to it, and the total price of the order can be calculated with or without applying a discount &this product class This class represents a product in an online store, with properties such as name, category, and price.

--- Onlinestore.java
+++ Onlinestore.java
@@ -1,4 +1,74 @@
 import java.util.Scanner;
+import java.util.*;
+
+class Product {
+    private String name;
+    private String category;
+    private double price;
+
+    public Product(String name, String category, double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
+class Order {
+    private Customer customer;
+    private List<Product> products;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double calculateTotal() {
+        double total = 0.0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+
+    public double applyDiscount() {
+        double total = calculateTotal();
+        if (total > 5000) {
+            double discount = 0.3 * total;
+            total -= discount;
+        }
+        return total;
+    }
+}
 
 class Customer {
     private String name;
@@ -27,6 +97,8 @@ public class Onlinestore {
         //creating a customer class and a new object to customer name  which will store name
         String customerName = scanner.nextLine();
         Customer customer = new Customer(customerName);
+        Order order = new Order(customer);
+
         
     }
  }
