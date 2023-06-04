52aa827a52d39977cc27f700d64318d33405c844
Anup Kumar M <anupkumarmofficial@gmail.com>
2023-06-04 16:37:19 +0530

    this code prompts the user to enter the name of a customer and creates a Customer object with the entered name.

--- Onlinestore.java
+++ Onlinestore.java
@@ -1,1 +1,32 @@
- 
+import java.util.Scanner;
+
+class Customer {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
+public class Onlinestore {
+    public static void main (String []args){
+        //importing a scanner from a built in package  and taking a string input from the user 
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the Name of the Customer: ");
+        //creating a customer class and a new object to customer name  which will store name
+        String customerName = scanner.nextLine();
+        Customer customer = new Customer(customerName);
+        
+    }
+ }
